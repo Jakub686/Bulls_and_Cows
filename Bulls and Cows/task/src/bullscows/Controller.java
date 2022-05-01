b36a69db265733315adfe4d368e209800fa06ba7
Jakub <jakub.bchyla@gmail.com>
2022-05-01 09:36:39 +0200

    refactored

--- Bulls and Cows/task/src/bullscows/Controller.java
+++ Bulls and Cows/task/src/bullscows/Controller.java
@@ -1,8 +1,11 @@
 package bullscows;
 
 import java.util.LinkedHashSet;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
+
+import static java.lang.Math.abs;
 
 public class Controller {
 
@@ -16,13 +19,15 @@ public class Controller {
 
     public static long pseudoRandom(int length) {
 
-        Long result = 0L;
+        long result = 0L;
         if (length >= 10) {
             System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
         } else {
 
-            long pseudoRandomNumber = System.nanoTime();
+            Random rand = new Random();
+            long pseudoRandomNumber = abs(rand.nextLong());
             String pseudoString = Long.toString(pseudoRandomNumber);
+
 
             char[] chars = pseudoString.toCharArray();
             Set<Character> charSet = new LinkedHashSet<Character>();
@@ -37,7 +42,7 @@ public class Controller {
             String st = sb.toString();
             st = st.substring(0, length);
             result = Long.parseLong(st);
-            System.out.println("The random secret number is " + result);
+
         }
         return result;
     }
@@ -45,40 +50,51 @@ public class Controller {
     public void run() {
         Scanner sc = new Scanner(System.in);
         Scanner ss = new Scanner(System.in);
+        boolean endCondition = true;
 
+        view.showEnter();
         int length = sc.nextInt();
         long random = Controller.pseudoRandom(length);
         String randomString = Long.toString(random);
+        do {
 
-        System.out.println("Enter number with " + length + " digits");  // czy wszystkei sout pchac do classy View?
-        String input = ss.nextLine();
+            view.showEnterNumberWithLength(length);
+            String input = ss.nextLine();
 
-        //bulls
-        for (int i = 0; i < randomString.length(); i++) {
-            if (input.charAt(i) == randomString.charAt(i)) {
-                model.setBulls(model.getBulls() + 1);
+            //bulls
+            for (int i = 0; i < randomString.length(); i++) {
+                if (input.charAt(i) == randomString.charAt(i)) {
+                    model.setBulls(model.getBulls() + 1);
 
-            }
-        }
-        //cows
-        for (int i = 0; i < randomString.length(); i++) {
-            for (int j = 0; j < randomString.length(); j++) {
-                if (input.charAt(i) == randomString.charAt(j)) {
-                    model.setCows(model.getCows() + 1);
                 }
             }
-        }
+            //cows
+            for (int i = 0; i < randomString.length(); i++) {
 
-        if (model.getBulls() > 0 & model.getCows() == 0)
-            view.showBulls(model.getBulls());
+                if (input.contains(Character.toString(randomString.charAt(i)))) {
+                    model.setCows(model.getCows() + 1);
+                }
 
-        if (model.getCows() > 0 & model.getBulls() == 0)
-            view.showCows(model.getCows());
+            }
 
-        if (model.getBulls() > 0 & model.getCows() > 0)
-            view.show(model.getBulls(), model.getCows());
+            if (model.getBulls() > 0 & model.getCows() == 0)
+                view.showBulls(model.getBulls());
 
-        if (model.getBulls() == 0 & model.getCows() == 0)
-            view.show();
+            if (model.getCows() > 0 & model.getBulls() == 0)
+                view.showCows(model.getCows());
+
+            if (model.getBulls() > 0 & model.getCows() > 0)
+                view.show(model.getBulls(), model.getCows());
+
+            if (model.getBulls() == 0 & model.getCows() == 0)
+                view.show();
+
+            if(model.getBulls() == length){
+                endCondition = false;
+            }
+            model.setCows(0);
+            model.setBulls(0);
+        }while(endCondition);
+        view.showWon(randomString);
     }
 }
--- Bulls and Cows/task/src/bullscows/View.java
+++ Bulls and Cows/task/src/bullscows/View.java
@@ -3,18 +3,30 @@ package bullscows;
 public class View {
 
     public void show() {
-        System.out.print("Grade: None. The secret code is");
+        System.out.print("Grade: None. Try again");
     }
 
     public void show(int bulls, int cows) {
-        System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is ");
+        int c = cows -  bulls;
+        System.out.println("Grade: " + bulls + " bull(s) and " + c + " cow(s). ");
     }
 
     public void showBulls(int bulls) {
-        System.out.println("Grade: " + bulls + " bull(s). The secret code is ");
+        System.out.println("Grade: " + bulls + " bull(s).");
     }
 
     public void showCows(int cows) {
-        System.out.println("Grade: " + cows + " cow(s). The secret code is ");
+        System.out.println("Grade: " + cows + " cow(s). ");
+    }
+
+    public void showEnter() {
+        System.out.println("Input the length of the secret code:");
+    }
+
+    public void showEnterNumberWithLength(int length) {
+        System.out.println("Enter number with " + length + " digits");
+    }
+    public void showWon(String randomString){
+        System.out.println("You win, the random secret number is " + randomString);
     }
 }
